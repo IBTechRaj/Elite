c3b83cddb7d13391398006ce5c564afad5d5dd5b
ibtechraj <raj_shk@rediffmail.com>
2020-12-10 08:35:29 +0530

    initial setup

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+/* eslint class-methods-use-this: ["error", { "exceptMethods": ["render"] }] */
+
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import "./App.css";
+
+import { HashRouter as Router, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+// import RecipesContainer from "../containers/RecipesContainer";
+// import Recipe from "../containers/Recipe";
+import Footer from "./Footer";
+import store from "./store";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="container-fluid">
+            <Navbar />
+            {/* <Route exact path="/" component={RecipesContainer} />
+            <Route exact path="/recipe/:id" component={Recipe} /> */}
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
+}
+export default App;
